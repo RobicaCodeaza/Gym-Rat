864ef25036146983fbbc15aafb0c0ec8ba7dd779
Stoica Robert <stoicarobert38@yahoo.com>
2024-09-17 23:46:37 +0300

    Config: Configured Hot Toast

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ import Calendar from './pages/Calendar'
 import Gym from './pages/Gym'
 import Equipment from './pages/Equipment'
 import Exercises from './pages/Exercises'
-
+import { Toaster } from 'react-hot-toast'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
-    defaultOptions: { queries: { retry: false, staleTime: 60 * 1000 } },
+    defaultOptions: { queries: { retry: false, staleTime: 0 } },
 })
 
 function App() {
@@ -82,6 +82,22 @@ function App() {
                         ></Route>
                     </Routes>
                 </BrowserRouter>
+                <Toaster
+                    position="top-center"
+                    gutter={12}
+                    containerStyle={{ margin: '8px' }}
+                    toastOptions={{
+                        success: { duration: 3000 },
+                        error: { duration: 5000 },
+                        style: {
+                            fontSize: '16px',
+                            maxWidth: '500px',
+                            padding: '16px 24px',
+                            backgroundColor: '#f6f6f6',
+                            color: '#11335a',
+                        },
+                    }}
+                ></Toaster>
             </QueryClientProvider>
         </>
     )
