76ff448b5512aed84f7609f42693b2597ddd194b
Stoica Robert <stoicarobert38@yahoo.com>
2024-09-19 00:03:17 +0300

    Config: Added Testing Library as Configuration

--- eslint.config.js
+++ eslint.config.js
@@ -18,7 +18,7 @@ export default tseslint.config(
             ...tseslint.configs.stylisticTypeChecked,
             vitest.configs.recommended,
             jestDom.configs['flat/recommended'],
-            // testingLibraryReact.configs['flat/react'],
+            testingLibrary.configs['flat/react'],
         ],
         files: ['**/*.{ts,tsx}'],
         languageOptions: {
@@ -37,7 +37,7 @@ export default tseslint.config(
         }, // What version of react we use to be available to the eslint-plugin-react
 
         plugins: {
-            'testing-library': testingLibrary,
+            // 'testing-library': testingLibrary,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
             react,
@@ -79,24 +79,24 @@ export default tseslint.config(
             //-----------------------------------------
             //testingLibraryReact Rules
 
-            'testing-library/await-async-events': [
-                'error',
-                { eventModule: 'userEvent' },
-            ],
-            'testing-library/await-async-queries': 'error',
-            'testing-library/await-async-utils': 'error',
-            'testing-library/no-dom-import': ['error', 'react'],
-            'testing-library/no-global-regexp-flag-in-query': 'error',
-            'testing-library/no-manual-cleanup': 'error',
-            'testing-library/no-node-access': 'error',
-            'testing-library/no-promise-in-fire-event': 'error',
-            'testing-library/no-unnecessary-act': 'error',
-            'testing-library/no-wait-for-multiple-assertions': 'error',
-            'testing-library/no-wait-for-side-effects': 'error',
-            'testing-library/no-wait-for-snapshot': 'error',
-            'testing-library/prefer-find-by': 'error',
-            'testing-library/prefer-presence-queries': 'error',
-            'testing-library/prefer-query-by-disappearance': 'error',
+            // 'testing-library/await-async-events': [
+            //     'error',
+            //     { eventModule: 'userEvent' },
+            // ],
+            // 'testing-library/await-async-queries': 'error',
+            // 'testing-library/await-async-utils': 'error',
+            // 'testing-library/no-dom-import': ['error', 'react'],
+            // 'testing-library/no-global-regexp-flag-in-query': 'error',
+            // 'testing-library/no-manual-cleanup': 'error',
+            // 'testing-library/no-node-access': 'error',
+            // 'testing-library/no-promise-in-fire-event': 'error',
+            // 'testing-library/no-unnecessary-act': 'error',
+            // 'testing-library/no-wait-for-multiple-assertions': 'error',
+            // 'testing-library/no-wait-for-side-effects': 'error',
+            // 'testing-library/no-wait-for-snapshot': 'error',
+            // 'testing-library/prefer-find-by': 'error',
+            // 'testing-library/prefer-presence-queries': 'error',
+            // 'testing-library/prefer-query-by-disappearance': 'error',
             //-----------------------------------------
         },
     }
