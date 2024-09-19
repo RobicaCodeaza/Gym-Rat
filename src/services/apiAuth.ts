991a5ff6be21a929741db7ff1968f01f5ffe9ae6
RobicaCodeaza <stoicarobert38@yahoo.com>
2024-09-19 20:38:54 +0300

    Feature/Auth: Added auth functions related to session,signout,signup

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,5 +1,18 @@
 import supabase from './supabase'
 
+export async function getCurrentSession() {
+    const { data: session } = await supabase.auth.getSession()
+
+    if (!session.session) return
+
+    const { data: userData, error: errorGettingUser } =
+        await supabase.auth.getUser()
+
+    if (errorGettingUser) throw new Error(errorGettingUser.message)
+
+    return userData.user
+}
+
 export async function login(email: string, password: string) {
     const { data, error } = await supabase.auth.signInWithPassword({
         email: email,
@@ -10,4 +23,21 @@ export async function login(email: string, password: string) {
         throw new Error(error.message)
     }
     return data
+}
+
+export async function signup(email: string, password: string) {
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+    })
+    if (error) {
+        throw new Error(error.message)
+    }
+    return data
+}
+
+export async function logout() {
+    const { error } = await supabase.auth.signOut()
+
+    if (error) throw new Error(error.message)
 }
