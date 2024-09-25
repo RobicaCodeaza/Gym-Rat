fb64edf869131ae7919efcd43db38cf14beb2bd2
Stoica Robert <stoicarobert38@yahoo.com>
2024-09-25 15:00:14 +0300

    Feature/Gym

--- src/features/gym/GymOverallView.tsx
+++ src/features/gym/GymOverallView.tsx
@@ -4,7 +4,7 @@ import WorkoutsStatus from './WorkoutsStatus'
 
 function GymOverallView() {
     return (
-        <div className="flex h-full flex-col gap-8 overflow-hidden tab-port:grid tab-port:grid-cols-[1fr_1fr] tab-port:gap-24">
+        <div className="flex h-full flex-col gap-8 overflow-hidden tab-port:grid tab-port:grid-cols-[1fr_1fr] tab-port:gap-20">
             <WorkoutsStatus></WorkoutsStatus>
             <ExercisesStatus></ExercisesStatus>
             <EquipmentStatus></EquipmentStatus>
