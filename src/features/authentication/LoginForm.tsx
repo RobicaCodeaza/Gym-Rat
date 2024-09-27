83444cd72e2c31730032602207ee2d21551415da
Stoica Robert <stoicarobert38@yahoo.com>
2024-09-27 16:28:25 +0300

    Feature/Ui - Configured FormRow

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -1,12 +1,12 @@
 import React, { Dispatch } from 'react'
 import { Label } from '@/components/aceternity/Label'
 import { Input } from '@/components/aceternity/Input'
-import { cn } from '@/lib/utils'
 import { IconBrandGoogle } from '@tabler/icons-react'
 import { useLogin } from './useLogin'
 import { type FieldErrors, type SubmitHandler, useForm } from 'react-hook-form'
 import Spinner from '../../ui/Spinner'
 import FullPage from '../../ui/FullPage'
+import { LabelInputContainer } from '../../ui/FormRow'
 
 type LoginProps = {
     toggleForm: Dispatch<React.SetStateAction<boolean>>
@@ -134,24 +134,5 @@ const BottomGradient = () => {
             <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
             <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
         </>
-    )
-}
-
-const LabelInputContainer = ({
-    children,
-    className,
-}: {
-    children: React.ReactNode
-    className?: string
-}) => {
-    return (
-        <div
-            className={cn(
-                'flex w-full flex-col space-y-2 tab-port:space-y-3',
-                className
-            )}
-        >
-            {children}
-        </div>
     )
 }
--- src/ui/FormRow.tsx
+++ src/ui/FormRow.tsx
@@ -3,6 +3,8 @@ import React, {
     ReactNode,
     type ReactElement,
 } from 'react'
+import { Label } from '../components/aceternity/Label'
+import { cn } from '../lib/utils'
 
 type FormRowProps = {
     children: ReactElement[] | ReactElement
@@ -23,7 +25,7 @@ function FormRow({ children, label, error, className }: FormRowProps) {
 
     return (
         <div
-            className={`border-mako-grey-100 flex flex-col justify-center gap-5 border-b py-4 has-[button]:flex has-[button]:flex-row has-[button]:justify-end has-[button]:gap-4 has-[button]:border-none phone:grid phone:grid-cols-[24rem_1fr_1.5fr] phone:items-center phone:gap-6 tab-port:grid-cols-[24rem_1fr_1fr] tab-port:gap-8 tab-land:grid-cols-[24rem_1fr_0.75fr] tab-land:gap-10 particular-small-laptop:grid-cols-[24rem_1fr_0.5fr] ${className}`}
+            className={`flex flex-col justify-center gap-5 border-b border-cod-gray-600 py-4 has-[button]:flex has-[button]:flex-row has-[button]:justify-end has-[button]:gap-4 has-[button]:border-none phone:grid phone:grid-cols-[24rem_1fr_1.5fr] phone:items-center phone:gap-6 tab-port:grid-cols-[24rem_1fr_1fr] tab-port:gap-8 tab-land:grid-cols-[24rem_1fr_0.75fr] tab-land:gap-10 particular-small-laptop:grid-cols-[24rem_1fr_0.5fr] ${className}`}
             style={{}}
         >
             {React.Children.map(children, (child) => {
@@ -33,14 +35,7 @@ function FormRow({ children, label, error, className }: FormRowProps) {
                 ) {
                     return (
                         <>
-                            {label && (
-                                <label
-                                    className="text-mako-grey-700 text-[1.4rem] tracking-wide"
-                                    htmlFor={child.props.id}
-                                >
-                                    {label}
-                                </label>
-                            )}
+                            {label && <Label htmlFor={child.props.id}></Label>}
                             {child}
                             {error && (
                                 <span className="flex text-center text-danger-500 phone:text-left">
@@ -51,6 +46,25 @@ function FormRow({ children, label, error, className }: FormRowProps) {
                     )
                 } else return <>{child}</>
             })}
+        </div>
+    )
+}
+
+export const LabelInputContainer = ({
+    children,
+    className,
+}: {
+    children: React.ReactNode
+    className?: string
+}) => {
+    return (
+        <div
+            className={cn(
+                'flex w-full flex-col space-y-2 tab-port:space-y-3',
+                className
+            )}
+        >
+            {children}
         </div>
     )
 }
