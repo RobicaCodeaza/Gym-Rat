10847f200171aee7033dd771c82a45e531040710
Stoica Robert <stoicarobert38@yahoo.com>
2024-09-30 00:03:13 +0300

    Feature/Gym: Workout Selector Added

--- src/features/gym/workout/WorkoutsTableOperations.tsx
+++ src/features/gym/workout/WorkoutsTableOperations.tsx
@@ -1,8 +1,20 @@
+import { useSearchParams } from 'react-router-dom'
 import Filter from '../../../ui/Filter'
+import Select from '../../../ui/Select'
 import SortBy from '../../../ui/SortBy'
 import TableOperations from '../../../ui/TableOperations'
 
 function WorkoutsTableOperations() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    function handleChange(
+        e: React.ChangeEvent<HTMLSelectElement>,
+        field: string
+    ) {
+        searchParams.set(field, e.target.value)
+        if (searchParams.get('page')) searchParams.set('page', '1')
+        setSearchParams(searchParams)
+    }
+
     return (
         <TableOperations>
             <Filter
@@ -13,6 +25,14 @@ function WorkoutsTableOperations() {
                     { value: 'notWorkedOut', label: 'Not Worked Out' },
                 ]}
             ></Filter>
+            <Select
+                onChange={(e) => handleChange(e, 'workoutType')}
+                options={[
+                    { value: 'push', label: 'Push' },
+                    { value: 'pull', label: 'Pull' },
+                ]}
+                currentValue=""
+            ></Select>
             <SortBy
                 options={[
                     {
--- src/ui/Select.tsx
+++ src/ui/Select.tsx
@@ -3,18 +3,18 @@ import { OptionsType } from './SortBy'
 
 type SelectProps = {
     options: OptionsType
-    currentSortBy: string
+    currentValue: string
 } & ComponentPropsWithoutRef<'select'>
 
 const Select = forwardRef<HTMLSelectElement, SelectProps>(function Select(
-    { options, currentSortBy, ...otherProps },
+    { options, currentValue, ...otherProps },
     ref
 ) {
     return (
         <select
             className="shadow-cod-gray/15 w-[18.5rem] rounded-lg border border-cod-gray-900 bg-cod-gray-950 px-2 py-2 text-[1.4rem] font-medium shadow-sm phone:px-3 phone:py-2 tab-land:w-auto"
             ref={ref}
-            value={currentSortBy}
+            value={currentValue}
             {...otherProps}
         >
             {options.map((option) => (
